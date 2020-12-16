9d9dd9b98b532d0ac96d5a4f26302fd6453e0150
Dobiks <49527911+Dobiks@users.noreply.github.com>
2020-12-16 21:44:15 +0100

    Update hack.cpp

--- hack.cpp
+++ hack.cpp
@@ -31,6 +31,7 @@ struct gameOffsets
 	DWORD dwEntityList = 0x4D9EAD4;
 	DWORD m_bDormant = 0xED;
 	DWORD m_iTeamNum = 0xF4;
+	DWORD m_iHealth = 0x100;;
 
 }offsets;
 
@@ -40,13 +41,14 @@ struct Player
 	float y;
 	DWORD isDormant;
 	DWORD team;
+	DWORD health;
 };
 
 std::vector<sf::CircleShape>add_circ(int cond)
 {
 	std::vector<sf::CircleShape>tmp;
 	if (cond == 0) {
-		for (int i = 0; i < 5; i++)
+		for (int i = 0; i < 16; i++)
 		{
 			sf::CircleShape shape(5);
 			shape.setFillColor(sf::Color(250, 0, 0));
@@ -55,10 +57,7 @@ std::vector<sf::CircleShape>add_circ(int cond)
 	}
 	else
 	{
-		sf::CircleShape shape(5);
-		shape.setFillColor(sf::Color(0, 0, 250)); //ally[0] <- 
-		tmp.push_back(shape);
-		for (int i = 0; i < 4; i++)
+		for (int i = 0; i < 16; i++)
 		{
 			sf::CircleShape shape(5);
 			shape.setFillColor(sf::Color(0, 250, 0));
@@ -77,8 +76,9 @@ int main()
 	sf::Texture t1;
 	t1.loadFromFile("mirage.png");
 	sf::Sprite s(t1);
+
 	std::vector<sf::CircleShape> enemy = add_circ(0);
-	std::vector<sf::CircleShape>ally = add_circ(1); //ally[0] <- localplayer
+	std::vector<sf::CircleShape>ally = add_circ(1); //a
 
 	val.process = MemClass.getProcess("csgo.exe");
 	val.gameModule = MemClass.getModule(val.process, "client.dll");
@@ -89,8 +89,10 @@ int main()
 
 	localPlayer.team = MemClass.readMem<DWORD>(val.localPlayer + offsets.m_iTeamNum);
 	//system("pause");
+
 	while (window.isOpen())
 	{
+
 		//exit
 		sf::Event e;
 		while (window.pollEvent(e))
@@ -103,8 +105,8 @@ int main()
 		//lc pos
 		localPlayer.x = MemClass.readMem<float>(val.localPlayer + offsets.m_vecOrigin);
 		localPlayer.y = MemClass.readMem<float>(val.localPlayer + offsets.m_vecOrigin + 0x4);
-		//localPlayer.x = 215 + localPlayer.x / 15;
-		//localPlayer.y = 110 + localPlayer.y / 15;
+		localPlayer.x = 215 + localPlayer.x / 15;
+		localPlayer.y = 110 + localPlayer.y / 15;
 
 		//ally[0].setPosition(localPlayer.x, localPlayer.y);
 
@@ -121,6 +123,7 @@ int main()
 			val.currentPlayer = MemClass.readMem<DWORD>(val.gameModule + offsets.dwEntityList + i * 0x10);
 			players_vector[i].isDormant = MemClass.readMem<DWORD>(val.currentPlayer + offsets.m_bDormant);
 			players_vector[i].team = MemClass.readMem<DWORD>(val.currentPlayer + offsets.m_iTeamNum);
+			players_vector[i].health = MemClass.readMem<DWORD>(val.currentPlayer + offsets.m_iHealth);
 
 			if (players_vector[i].isDormant == 0)
 			{
@@ -129,21 +132,24 @@ int main()
 				players_vector[i].x = 215 + players_vector[i].x / 15;
 				players_vector[i].y = 110 - players_vector[i].y / 15;
 
-				if((players_vector[i].x<315 || players_vector[i].x > 34) && (players_vector[i].y < 300 || players_vector[i].y > 40))
+				if (licznik_ally < 15 && licznik_enemy < 15) 
 				{
-					if (players_vector[i].team == localPlayer.team || licznik_ally < 5)
+					if (players_vector[i].team == 3 && players_vector[i].health > 0)
 					{
 						//ally
+
 						ally[licznik_ally].setPosition(players_vector[i].x, players_vector[i].y);
 						licznik_ally++;
 					}
-					else if (players_vector[i].team != localPlayer.team || licznik_enemy < 5)
+					else if (players_vector[i].team == 2 && players_vector[i].health > 0)
 					{
 						//enemy
 						enemy[licznik_enemy].setPosition(players_vector[i].x, players_vector[i].y);
 						licznik_enemy++;
+
 					}
 				}
+
 			}
 		}
 		//set pos
@@ -154,14 +160,25 @@ int main()
 		window.draw(s);
 		for (auto el : enemy)
 		{
-			window.draw(el);
+			if (el.getPosition().x != 0 || el.getPosition().y != 0)
+			{
+				window.draw(el);
+			}
 		}
 		for (auto el : ally)
 		{
-			window.draw(el);
+			if (el.getPosition().x != 0 || el.getPosition().y != 0)
+			{
+				if (el.getPosition().x == localPlayer.x)
+				{
+					el.setFillColor(sf::Color(1, 1, 250));
+				}
+				window.draw(el);
+			}
 		}
 		window.display();
 		Sleep(1);
 	}
+	
 	return 0;
 }
